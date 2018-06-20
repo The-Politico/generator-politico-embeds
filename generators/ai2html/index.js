ed47909daf7faf3826207295b1b9d42e9768b5b9
Andrew Briz <briz.andrew@gmail.com>
2018-06-20 10:04:58 -0400

    Added ai2html resizer to generator.

--- generators/ai2html/index.js
+++ generators/ai2html/index.js
@@ -5,5 +5,8 @@ module.exports = class extends Generator {
     this.fs.copy(
       this.templatePath('src/ai/config.yml'),
       this.destinationPath('src/ai/config.yml'));
+    this.fs.copy(
+      this.templatePath('src/templates/_ai2html-resizer.html'),
+      this.destinationPath('src/templates/_ai2html-resizer.html'));
   }
 };
